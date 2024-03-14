1050523fe55d3871746bfa11b77ffd00b7a8ed8e
PaoDev <126527997+PaoSotomayor@users.noreply.github.com>
2024-03-14 16:52:04 -0500

    Add files via upload

---
+++ contactAPI/src/main/java/com/contactAPI/entities/Contact.java
@@ -0,0 +1,32 @@
+package com.contactAPI.entities;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+@Table(name = "contacts")
+public class Contact {
+    @Id
+    @GeneratedValue(generator = "uuid-hibernate-generator")
+    @GenericGenerator(name = "uuid-hibernate-generator", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", unique = true, updatable = false)
+    private String id;
+    private String name;
+    private String email;
+    private String title;
+    private String phone;
+    private String address;
+    private String status;
+    private String photoURL;
+}
---
+++ contactAPI/src/main/java/com/contactAPI/repo/ContactRepo.java
@@ -0,0 +1,12 @@
+package com.contactAPI.repo;
+
+import com.contactAPI.entities.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ContactRepo extends JpaRepository <Contact, String> {
+    Optional <Contact> findById (String id);
+}
